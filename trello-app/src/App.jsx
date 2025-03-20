b266fded80e89d919c55c7bca588c5e0a11bf0b2
Long Nguyen <longwinpro113@gmail.com>
2025-03-20 15:44:02 +0700

    change eslint to initial rules, add theme and color

--- trello-app/src/App.jsx
+++ trello-app/src/App.jsx
@@ -1,30 +1,27 @@
-import Button from "@mui/material/Button";
-import AccessAlarmIcon from "@mui/icons-material/AccessAlarm";
-import ThreeDRotation from "@mui/icons-material/ThreeDRotation";
-import HomeIcon from "@mui/icons-material/Home";
-import { pink } from "@mui/material/colors";
+import Button from '@mui/material/Button'
+import HomeIcon from '@mui/icons-material/Home'
+import { pink } from '@mui/material/colors'
+import Typography  from '@mui/material/Typography'
 
 function App() {
-    return (
-        <>
-            <div>Hello World</div>
-            <Button variant="text">Text</Button>
-            <Button variant="contained">Contained</Button>
-            <Button variant="outlined">Outlined</Button>
+  return (
+    <>
+      <Typography variant="body2" color="text.secondary">Hello World</Typography>
+      {/* <div>Hello World</div> */}
+      <Button variant="text">Text</Button>
+      <Button variant="contained">Contained</Button>
+      <Button variant="outlined">Outlined</Button>
 
-            <br/>
-            <AccessAlarmIcon />
-            <ThreeDRotation />
-            <br />
-            <HomeIcon />
-            <HomeIcon color="primary" />
-            <HomeIcon color="secondary" />
-            <HomeIcon color="success" />
-            <HomeIcon color="action" />
-            <HomeIcon color="disabled" />
-            <HomeIcon sx={{ color: pink[100] }} />
-        </>
-    );
+      <br />
+      <HomeIcon />
+      <HomeIcon color="primary" />
+      <HomeIcon color="secondary" />
+      <HomeIcon color="success" />
+      <HomeIcon color="action" />
+      <HomeIcon color="disabled" />
+      <HomeIcon sx={{ color: pink[100] }} />
+    </>
+  )
 }
 
-export default App;
+export default App
