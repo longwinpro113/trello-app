6ecddba3457a0693d713afe6b20190329ee0335e
Long Nguyen <longwinpro113@gmail.com>
2025-03-21 16:18:57 +0700

    add toggle mode light/dark

--- trello-app/src/App.jsx
+++ trello-app/src/App.jsx
@@ -2,10 +2,26 @@ import Button from '@mui/material/Button'
 import HomeIcon from '@mui/icons-material/Home'
 import { pink } from '@mui/material/colors'
 import Typography  from '@mui/material/Typography'
+import { useColorScheme } from '@mui/material/styles'
+
+function ModeToggle() {
+  const { mode, setMode } = useColorScheme()
+  return (
+    <Button onClick={() => {
+      setMode(mode === 'light' ? 'dark' : 'light')
+    }}>
+      
+      {mode === 'light' ? 'Turn dark' : 'Turn light'}
+    </Button>
+  )
+}
 
 function App() {
   return (
     <>
+      <ModeToggle/>
+      <hr/>
+      <div>Hello Long</div>
       <Typography variant="body2" color="text.secondary">Hello World</Typography>
       {/* <div>Hello World</div> */}
       <Button variant="text">Text</Button>
--- trello-app/src/theme.js
+++ trello-app/src/theme.js
@@ -1,23 +1,21 @@
-import { createTheme } from '@mui/material/styles'
-import { red } from '@mui/material/colors'
+import { experimental_extendTheme as extendTheme } from '@mui/material/styles'
+import { teal, deepOrange, cyan, orange as Orange } from '@mui/material/colors'
 
 // Create a theme instance.
-const theme = createTheme({
-  cssVariables: true,
-  palette: {
-    mode: 'dark', // Default mode is light
+const theme = extendTheme({
+  colorSchemes: {
+    light: {
+      palette: {
+        primary: teal,
+        secondary: deepOrange,
+      }
+    },
 
-    primary: {
-      main: '#556cd6'
-    },
-    secondary: {
-      main: '#19857b'
-    },
-    error: {
-      main: red.A400
-    },
-    text: {
-        secondary: red[500]
+    dark: {
+      palette: {
+        primary: cyan,
+        secondary: Orange
+      }
     }
   }
 })
