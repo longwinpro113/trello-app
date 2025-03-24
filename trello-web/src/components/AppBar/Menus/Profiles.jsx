e0d4f1722f409f0db578b9be5f48c4981e6073ee
Long Nguyen <longwinpro113@gmail.com>
2025-03-24 11:20:33 +0700

    Customize app bar

---
+++ trello-web/src/components/AppBar/Menus/Profiles.jsx
@@ -0,0 +1,90 @@
+import React from 'react'
+import Box from '@mui/material/Box'
+import Menu from '@mui/material/Menu'
+import MenuItem from '@mui/material/MenuItem'
+import Divider from '@mui/material/Divider'
+import ListItemIcon from '@mui/material/ListItemIcon'
+import Avatar from '@mui/material/Avatar'
+import IconButton from '@mui/material/IconButton'
+import Tooltip from '@mui/material/Tooltip'
+import PersonAdd from '@mui/icons-material/PersonAdd'
+import Settings from '@mui/icons-material/Settings'
+import Logout from '@mui/icons-material/Logout'
+
+function Profiles() {
+    const [anchorEl, setAnchorEl] = React.useState(null)
+    const open = Boolean(anchorEl)
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget)
+    }
+    const handleClose = () => {
+        setAnchorEl(null)
+    }
+
+    return (
+        <Box>
+            <Tooltip title="Account settings">
+                <IconButton
+                    onClick={handleClick}
+                    size="small"
+                    sx={{ padding: 0 }}
+                    aria-controls={open ? 'basic-button-profiles' : undefined}
+                    aria-haspopup="true"
+                    aria-expanded={open ? 'true' : undefined}>
+
+                    <Avatar sx={{ width: 32, height: 32 }}
+                        src='https://cdn.discordapp.com/avatars/820696454678315090/4e3000407dffe24404a326f46ecb2ac5.webp?size=160'
+                        alt='My Profile Pic' />
+
+                </IconButton>
+            </Tooltip>
+
+            <Menu
+                id="basic-menu-profiles"
+                // aria-labelledby="demo-positioned-button"
+                anchorEl={anchorEl}
+                open={open}
+                onClose={handleClose}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'left',
+                }}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button-profiles'
+                }}
+            >
+                <MenuItem>
+                    <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> Profile
+                </MenuItem>
+                <MenuItem >
+                    <Avatar sx={{ width: 28, height: 28, mr: 2 }} /> My account
+                </MenuItem>
+                <Divider />
+                <MenuItem>
+                    <ListItemIcon>
+                        <PersonAdd fontSize="small" />
+                    </ListItemIcon>
+                    Add another account
+                </MenuItem>
+                <MenuItem>
+                    <ListItemIcon>
+                        <Settings fontSize="small" />
+                    </ListItemIcon>
+                    Settings
+                </MenuItem>
+                <MenuItem>
+                    <ListItemIcon>
+                        <Logout fontSize="small" />
+                    </ListItemIcon>
+                    Logout
+                </MenuItem>
+            </Menu>
+        </Box>
+    )
+}
+
+export default Profiles
