dc4bacf12e64ff08f42dfdb58b1b7a8267ed4db0
Long Nguyen <longwinpro113@gmail.com>
2025-03-25 15:47:24 +0700

    AppBar theme override

--- trello-web/src/components/AppBar/index.jsx
+++ trello-web/src/components/AppBar/index.jsx
@@ -30,7 +30,7 @@ function AppBar() {
         <AppsIcon sx={{ color: 'primary.main' }} />
 
         <Box sx={{ display: 'flex', alignItems: 'center', gap: 0.5 }}>
-          <SvgIcon component={TrelloIcon} inheritViewBox sx={{ color: 'primary.main' }} />
+          <SvgIcon component={TrelloIcon} fontSize='small' inheritViewBox sx={{ color: 'primary.main' }} />
 
           <Typography variant='span' sx={{ fontSize: '1.2rem', fontWeight: 'bold', color: 'primary.main' }}>
             Trello
@@ -51,17 +51,17 @@ function AppBar() {
 
         <ModeSelect />
 
-        <Tooltip title="Delete">
+        <Tooltip title="Notifications">
           <Badge color="secondary" variant="dot" sx={{ cursor: 'pointer' }}>
-            <NotificationsNoneIcon />
+            <NotificationsNoneIcon sx={{ color: 'primary.main' }} />
           </Badge>
         </Tooltip>
 
         <Tooltip title="Help">
-            <HelpOutlineIcon sx={{ cursor: 'pointer' }}/>
+          <HelpOutlineIcon sx={{ cursor: 'pointer', color: 'primary.main' }} />
         </Tooltip>
 
-        <Profiles/>
+        <Profiles />
 
       </Box>
     </Box>
